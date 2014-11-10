d1d887c6baea1e7b7136119a9ba6014096897e37
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2014-11-09 23:45:27 -0600

    further document source code methods

--- js/xcon.js
+++ js/xcon.js
@@ -33,8 +33,16 @@
     // skins a console.log message to display the primitive type as well
     // as the expected output of a vanilla .log() command
     // opts: {
-    //     "color": "#f9f9f9", // specify a color output
+    //     "color": "#f9f9f9", // specify a css color
     //     "log": true //
+    // }
+    // sometimes this function is called from console.run() to output
+    // function return values.  In that case,
+    // opts: {
+    //     "color": "darkgreen", // or red
+    //     "fnName": "nameOfFunction",
+    //     "fnArgs": "all passed arguments",
+    //     "error": true // indicates a thrown error
     // }
     this.out = function (rawText, opts) {
 
@@ -45,7 +53,7 @@
             typeColor = opts.color || 'gray';
         type = opts.error ? 'error' : type;
         type = opts.fnName ? opts.fnName + '(' +
-            opts.fnArgs.toString() + ') returns ' + type : type;
+            opts.fnArgs + ') returns ' + type : type;
         console.log("%c" + type, "color:" + typeColor + ";font-weight:bold;");
         console.log("%c" + text, "color:" + opts.color + ";font-weight:bold;");
         if (opts.log) {
@@ -53,6 +61,10 @@
         }
     };
 
+    // given a function, calls that function, returns the output,
+    // and calls .out() to skin the output in green (success)
+    // or red (failure).  Arguments, function name, and any error
+    // messages are logged.
     this.run = function (fn) {
         if (typeof fn === 'function') {
             var args = [], n, result;
@@ -64,14 +76,14 @@
                 this.out(result, {
                     "color": 'darkgreen',
                     "fnName": fn.name || 'anonymous function',
-                    "fnArgs": args
+                    "fnArgs": args.toString()
                 });
                 return result;
             } catch (e) {
                 this.out(e.message, {
                     "color": 'red',
                     "fnName": fn.name || 'anonymous function',
-                    "fnArgs": args,
+                    "fnArgs": args.toString(),
                     "error": true
                 });
             }
