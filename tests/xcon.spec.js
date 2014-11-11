93f8e88fed4c422b0e7e0bc1a973ceef1166ad8c
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2014-11-10 20:03:22 -0600

    update spec to drop the trailing ":" from condition()

--- tests/xcon.spec.js
+++ tests/xcon.spec.js
@@ -30,7 +30,7 @@ describe('condition()', function () {
                 text = 'undefined';
                 break;
         }
-        type = type + ':';
+        type = type;
         return {
             "type": type,
             "text": text
@@ -39,7 +39,7 @@ describe('condition()', function () {
 
 	it('should return .text and .type', function () {
 		var foundType = condition(Math.PI);
-		expect(foundType.type).toEqual('number:');
+		expect(foundType.type).toEqual('number');
 		expect(foundType.text).toEqual('3.141592653589793');
 	});
 
@@ -53,45 +53,45 @@ describe('condition()', function () {
 	});
 
 	it('should correctly identify numbers', function () {
-		expect(condition(4).type).toEqual('number:');
-		expect(condition(NaN).type).toEqual('number:');
-		expect(condition("4").type).not.toEqual('number:');
+		expect(condition(4).type).toEqual('number');
+		expect(condition(NaN).type).toEqual('number');
+		expect(condition("4").type).not.toEqual('number');
 	});
 
 	it('should correctly identify booleans', function () {
-		expect(condition(false).type).toEqual('boolean:');
-		expect(condition(true).type).toEqual('boolean:');
-		expect(condition('true').type).not.toEqual('boolean:');
+		expect(condition(false).type).toEqual('boolean');
+		expect(condition(true).type).toEqual('boolean');
+		expect(condition('true').type).not.toEqual('boolean');
 	});
 
 	it('should correctly identify strings', function () {
-		expect(condition('Steve Perry').type).toEqual('string:');
-		expect(condition(['Steve', 123][1]).type).not.toEqual('string:');
+		expect(condition('Steve Perry').type).toEqual('string');
+		expect(condition(['Steve', 123][1]).type).not.toEqual('string');
 	});
 
 	it('should be able to identify undefined', function () {
-		expect(condition(undefined).type).toEqual('undefined:');
-		expect(condition('undefined').type).not.toEqual('undefined:');
+		expect(condition(undefined).type).toEqual('undefined');
+		expect(condition('undefined').type).not.toEqual('undefined');
 	});
 
 	it('should correctly identify objects', function () {
-		expect(condition({"name": "value"}).type).toEqual('object:');
+		expect(condition({"name": "value"}).type).toEqual('object');
 	});
 
 	it('should correctly identify arrays', function () {
-		expect(condition(["one", "two", 3]).type).toEqual('array:');
+		expect(condition(["one", "two", 3]).type).toEqual('array');
 	});
 
 	it('should be able to identify null', function () {
-		expect(condition(null).type).toEqual('null:');
-		expect(condition(undefined).type).not.toEqual('null:');
-		expect(condition('null').type).not.toEqual('null:');
+		expect(condition(null).type).toEqual('null');
+		expect(condition(undefined).type).not.toEqual('null');
+		expect(condition('null').type).not.toEqual('null');
 	});
 
 	it('should differentiate between objects, arrays, and null', function () {
-		expect(condition([1, 2, 3]).type).not.toEqual('object:');
-		expect(condition({"an": "object"}).type).not.toEqual('array:');
-		expect(condition(null).type).not.toEqual('object:');
+		expect(condition([1, 2, 3]).type).not.toEqual('object');
+		expect(condition({"an": "object"}).type).not.toEqual('array');
+		expect(condition(null).type).not.toEqual('object');
 	});
 
 });
