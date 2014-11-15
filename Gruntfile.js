a6a46e5fed2c90bd0197441d266b708d551968c2
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2014-11-15 15:55:01 -0600

    update bower file, include gruntfile so that devs use consistent workflow

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        "uglify": {
+            "js": {
+                "files": {
+                    'dist/xcon-min.js': ['build/xcon.js']
+                }
+            }
+        },
+        "requirejs": {
+            "js": {
+                "options": {
+                    'findNestedDependencies': true,
+                    'baseUrl': 'src/js',
+                    'optimize': 'none',
+                    'mainConfigFile': 'src/config/config.js',
+                    'include': ['main.js'],
+                    'out': 'build/xcon.js',
+                    'onModuleBundleComplete': function (data) {
+                        var fs = require('fs'),
+                        amdclean = require('amdclean'),
+                        outputFile = data.path;
+
+                        fs.writeFileSync(outputFile, amdclean.clean({
+                            'filePath': outputFile
+                        }));
+                    }
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('build', ['requirejs:js', 'uglify:js']);
+};
