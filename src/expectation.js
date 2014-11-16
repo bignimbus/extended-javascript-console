3a8790606aa7e3cbd1b19ed17753a4272d164e25
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2014-11-16 14:24:15 -0600

    add .not, .toBeTruthy and .toBeFalsy

--- src/expectation.js
+++ src/expectation.js
@@ -2,11 +2,25 @@ define([],
 function () {
 	"use strict";
 	function Expectation (thing) {
-		this.toEqual = function (otherThing) {
-			if (thing === otherThing) {
-				return true;
-			}
-			return false;
+		this.toEqual = function (otherThing, not) {
+			return not ? thing !== otherThing : thing === otherThing;
+		};
+		this.toBeTruthy = function (not) {
+			return not ? !thing : !!thing;
+		};
+		this.toBeFalsy = function (not) {
+			return not ? !!thing : !thing;
+		};
+		this.not = {
+			"toEqual": function (otherThing) {
+				return this.toEqual(otherThing, true);
+			}.bind(this),
+			"toBeTruthy": function () {
+				return this.toBeTruthy(true);
+			}.bind(this),
+			"toBeFalsy": function () {
+				return this.toBeFalsy(true);
+			}.bind(this)
 		};
 		return this;
 	}
