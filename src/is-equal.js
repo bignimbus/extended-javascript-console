5a5ebb5e6af67f392c420f0684d66e81d1c26653
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2014-11-16 23:11:20 -0600

    add module for comparing all types of data, including objects, arrays and functions

---
+++ src/is-equal.js
@@ -0,0 +1,41 @@
+// based on a solution by Ebrahim Byagowi.  Original code can be found at:
+// http://stackoverflow.com/questions/201183/how-to-determine-equality-for-two-javascript-objects/16788517#16788517
+
+define([], function () {
+    "use strict";
+    function isEqual (thing, otherThing) {
+        if (thing instanceof Function) {
+            if (otherThing instanceof Function) {
+                return thing.toString() === otherThing.toString();
+            }
+            return false;
+        }
+        if (thing === null || thing === void 0 || otherThing === null || otherThing === void 0) {
+            return thing === otherThing;
+        }
+        if (thing === otherThing || thing.valueOf() === otherThing.valueOf()) {
+            return true;
+        }
+
+        // if one of them is date, they must had equal valueOf
+        if (thing instanceof Date || otherThing instanceof Date) {
+            return false;
+        }
+
+        // if they are not function or strictly equal, they both need to be Objects
+        if (!(thing instanceof Object) || !(otherThing instanceof Object)) {
+            return false;
+        }
+
+        var thingKeys = Object.keys(thing),
+            otherKeys = Object.keys(otherThing),
+            equal = otherKeys.every(function (key) {
+                    return thingKeys.indexOf(key) !== -1;
+                }) &&
+                thingKeys.every(function (key) {
+                    return isEqual(thing[key], otherThing[key]);
+                });
+        return equal;
+    }
+    return isEqual;
+});
