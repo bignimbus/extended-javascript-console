7d6b846e8ba47a1fbfb3ed7a58be8fab3d596b04
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2014-11-16 23:13:52 -0600

    add toBeDefined, move .not to main module, initialize options object, call is-equal module for complex data comparisons

--- src/expectation.js
+++ src/expectation.js
@@ -1,26 +1,25 @@
-define([],
-function () {
+define(['is-equal'],
+function (isEqual) {
 	"use strict";
-	function Expectation (thing) {
-		this.toEqual = function (otherThing, not) {
-			return not ? thing !== otherThing : thing === otherThing;
+	function Expectation (thing, opts) {
+		opts = opts || {};
+		var not = opts.not || false;
+		this.toEqual = function (otherThing) {
+			var result = isEqual(thing, otherThing);
+			result = not ? !result : result;
+			return result;
 		};
-		this.toBeTruthy = function (not) {
-			return not ? !thing : !!thing;
+		this.toBeTruthy = function () {
+			var result = not ? !thing : !!thing;
+			return result;
 		};
-		this.toBeFalsy = function (not) {
-			return not ? !!thing : !thing;
+		this.toBeFalsy = function () {
+			var result = not ? !!thing : !thing;
+			return result;
 		};
-		this.not = {
-			"toEqual": function (otherThing) {
-				return this.toEqual(otherThing, true);
-			}.bind(this),
-			"toBeTruthy": function () {
-				return this.toBeTruthy(true);
-			}.bind(this),
-			"toBeFalsy": function () {
-				return this.toBeFalsy(true);
-			}.bind(this)
+		this.toBeDefined = function () {
+			var result = not ? thing === void 0 : thing !== void 0;
+			return result;
 		};
 		return this;
 	}
